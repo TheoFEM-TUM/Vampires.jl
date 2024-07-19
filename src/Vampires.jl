aaf75b704320994a2a4375d1423cf4ab67f04b11
Jonas <joldenstaedt@gmail.com>
2024-07-19 18:31:01 +0200

    added comments/sections to module file

--- src/Vampires.jl
+++ src/Vampires.jl
@@ -3,19 +3,24 @@ module Vampires
 using OrderedCollections, ArgParse, Plots, BenchmarkTools, CSV, LinearAlgebra
 
 include("read_utils.jl")
+# input
 include("io/input/eigenval.jl"); include("io/input/doscar.jl"); include("io/input/poscar.jl"); include("io/input/xdatcar.jl")
 include("io/input/incar.jl")
 
+# output
 include("io/output/incar.jl")
 
+# calculations
 include("calculations/energy/bandstructure.jl")
 include("calculations/xdos/dos.jl"); # include("calculations/xdos/jdos.jl")
 
+# plotting
+include("plotting/energy/bandstructure.jl"); include("plotting/xdos/dos.jl"); 
+
+# tasks
 include("tasks/modify_task.jl"); include("tasks/calculation_task.jl"); include("tasks/plot_task.jl"); include("tasks/convergence_task.jl")
 
 include("cli_interface.jl")
-
-include("plotting/energy/bandstructure.jl"); include("plotting/xdos/dos.jl"); 
 
 export read_eigenval, read_doscar, read_incar, set_keyword!, write_incar, Poscar, read_poscar, write_poscar, read_xdatcar
 export get_value_for_keyword, change_incar!
