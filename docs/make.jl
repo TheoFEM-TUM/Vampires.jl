53f11961599e6141f7561a9bfb13f41d66240b46
Jonas <joldenstaedt@gmail.com>
2024-07-26 14:31:37 +0200

    made docs its own project

--- docs/make.jl
+++ docs/make.jl
@@ -2,9 +2,16 @@ using Documenter
 using Vampires
 
 makedocs(
-    sitename = "Vampires.jl",
-    format = Documenter.HTML(),
     modules = [Vampires],
+    sitename = "Vampires.jl",
+    format = Documenter.HTML(;
+    # Use clean URLs, unless built as a "local" build
+    prettyurls=CONTINUOUS_INTEGRATION,
+    canonical="https://docs.dftk.org/stable/",
+    edit_link="master",
+    size_threshold=nothing,  # do not fail build if large HTML outputs
+    mathengine,
+    ),
     pages = [
         "Home" => "index.md",
         "Manual" => [
