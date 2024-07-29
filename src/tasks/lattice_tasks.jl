79b5a5e5fd984a38e9da81634ec0ff1ea47e424d
martin <ga63loc@mytum.de>
2024-07-29 11:20:54 +0200

    added task for supercell creation

---
+++ src/tasks/lattice_tasks.jl
@@ -0,0 +1,15 @@
+"""
+list of available tasks:
+
+supercell
+    create: create a supercell POSCAR file from an existing POSCAR file
+"""
+
+
+function run_task(::Type{Val{:supercell}}, ::Type{Val{:create}}, args)
+    poscar = read_poscar(args["p"]*args["poscar"])
+    N = occursin(',', args["N"]) ? split_line(args["N"], char=',') : args["N"]
+    N = parse.(Int64, N)
+    sc_poscar = multiply_primitive_cell(poscar, N)
+    write_poscar(sc_poscar, filename=args["p"]*"SC_POSCAR") #TODO: args["o"]?
+end
