6d45c8e87337efdaa3c65eae10b29d4a079236f9
martin <ga63loc@mytum.de>
2024-07-30 10:00:42 +0200

    tests for nscf folder structure

--- test/io/output/test_folder_management.jl
+++ test/io/output/test_folder_management.jl
@@ -1,12 +1,32 @@
 path = test_file_path*"param_test/"
 
-keyword = "ENCUT"
-values = ["300", "350", "400"]
-convergence_create_subdirectories(keyword, values; path=path, verbose=false)
+@testset "convergence" begin
+    keyword = "ENCUT"
+    values = ["300", "350", "400"]
+    convergence_create_subdirectories(keyword, values; path=path, verbose=false)
+    for (folder, value) in zip(keyword * "_" .* values, values)
+        @test "INCAR" in readdir(path*folder) && "KPOINTS" in readdir(path*folder) && "POSCAR" in readdir(path*folder) && "POTCAR" in readdir(path*folder)
+        incar_ = read_incar(path*folder*"/INCAR")
+        @test get_value_for_keyword(keyword, incar_) == value
+        rm(path*folder, recursive=true)
+    end
+end
 
-for (folder, value) in zip(keyword * "_" .* values, values)
-    @test "INCAR" in readdir(path*folder) && "KPOINTS" in readdir(path*folder) && "POSCAR" in readdir(path*folder) && "POTCAR" in readdir(path*folder)
-    incar_ = read_incar(path*folder*"/INCAR")
-    @test get_value_for_keyword(keyword, incar_) == value
-    rm(path*folder, recursive=true)
+@testset "NSCF" begin
+    nscf_create_subdirectories(path, "KPOINTS,KPOINTS_bands", verbose=false)
+    scf_incar = read_incar(path*"scf/INCAR")
+    @test get_value_for_keyword("ISTART", scf_incar) == "0"
+    @test get_value_for_keyword("LCHARG", scf_incar) == "True"
+
+    nscf_incar = read_incar(path*"nscf/INCAR")
+    @test get_value_for_keyword("ICHARG", nscf_incar) == "11"
+    @test get_value_for_keyword("LCHARG", nscf_incar) == "False"
+
+    @test Vampires.open_and_read(path*"scf/POSCAR") == Vampires.open_and_read(path*"nscf/POSCAR")
+    @test Vampires.open_and_read(path*"scf/POTCAR") == Vampires.open_and_read(path*"nscf/POTCAR")
+    @test Vampires.open_and_read(path*"scf/KPOINTS") ≠ Vampires.open_and_read(path*"nscf/KPOINTS")
+    @test Vampires.open_and_read(path*"scf/KPOINTS") == Vampires.open_and_read(path*"KPOINTS")
+    @test Vampires.open_and_read(path*"nscf/KPOINTS") == Vampires.open_and_read(path*"KPOINTS_bands")
+
+    rm(path*"scf", recursive=true); rm(path*"nscf", recursive=true)
 end
