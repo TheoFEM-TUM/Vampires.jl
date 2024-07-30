aac6d4ecb7302b6bfe813a5b1085d96090b15631
martin <ga63loc@mytum.de>
2024-07-30 10:00:16 +0200

    added available tasks docstring

--- src/tasks/nscf_task.jl
+++ src/tasks/nscf_task.jl
@@ -1,6 +1,19 @@
+"""
+list of available tasks:
+
+nscf
+    make: generate the folder structure for a scf->nscf calculation
+"""
+
+
+
+
+
+
+
 function run_task(::Type{Val{:nscf}}, ::Type{Val{:make}}, args)
     nscf_create_subdirectories(args["p"], args["kpoints"])
-    filename = "run_nscf.sh"
+    filename = args["p"]*"run_nscf.sh"
     write_run_script(args["vasp_exe"], args["p"], cb="ln scf/CHGCAR nscf/CHGCAR", out=filename)
     add_path_to_folders.(filename, ["scf", "nscf"])
 end
