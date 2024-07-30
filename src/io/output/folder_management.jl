95c75687649ac4d51a8a7b79d6fb089bbfe39f6c
martin <ga63loc@mytum.de>
2024-07-30 10:00:30 +0200

    added function to generate nscf folder structure

--- src/io/output/folder_management.jl
+++ src/io/output/folder_management.jl
@@ -12,7 +12,7 @@ function convergence_create_subdirectories(param, param_range; path="./", verbos
     for value in param_range
         folder = param*"_"*value
         mkpath(path*folder)
-        copy_vasp_input(path, folder)
+        copy_vasp_input(path, folder, ignore=["INCAR"])
         set_keyword_in_incar!(param, value, path*"INCAR", out=path*folder*"/INCAR", verbose=verbose)
     end
 end
@@ -27,21 +27,21 @@ and adjusting INCAR settings.
 - `path::String`: The directory where the VASP input files are located.
 - `kpoint_files::String`: A string containing two KPOINTS filenames for the "scf" and "nscf" calculations respectively.
 """
-function nscf_create_subdirectories(path, kpoints)
+function nscf_create_subdirectories(path, kpoints; verbose=true)
     for folder in ["scf", "nscf"]
-        mkdir(folder)
+        mkdir(path*folder)
         copy_vasp_input(path, folder, ignore=["KPOINTS"])
     end
     kpoint_files = split_line(kpoints, char=',')
-    cp(path*kpoint_files[1], path*"scf"); cp(path*kpoint_files[2], path*"nscf") # TODO: write kpoint file with from kpath argument?
+    cp(path*kpoint_files[1], path*"scf/KPOINTS"); cp(path*kpoint_files[2], path*"nscf/KPOINTS") # TODO: write kpoint file with from kpath argument?
 
-    remove_keyword_from_incar!("LCHARG", path*"scf/INCAR")
-    set_keyword_in_incar!("ISTART", "0", path*"scf/INCAR")
-    set_keyword_in_incar!("LCHARG", "True", path*"scf/INCAR")
+    remove_keyword_from_incar!("LCHARG", path*"scf/INCAR", verbose=verbose)
+    set_keyword_in_incar!("ISTART", "0", path*"scf/INCAR", verbose=verbose)
+    set_keyword_in_incar!("LCHARG", "True", path*"scf/INCAR", verbose=verbose)
 
-    remove_keyword_from_incar!("ISTART", path*"nscf/INCAR")
-    set_keyword_in_incar!("ICHARG", "11", path*"nscf/INCAR")
-    set_keyword_in_incar!("LCHARG", "False", path*"nscf/INCAR")
+    remove_keyword_from_incar!("ISTART", path*"nscf/INCAR", verbose=verbose)
+    set_keyword_in_incar!("ICHARG", "11", path*"nscf/INCAR", verbose=verbose)
+    set_keyword_in_incar!("LCHARG", "False", path*"nscf/INCAR", verbose=verbose)
 end
 
 """
@@ -55,10 +55,10 @@ Copy specific VASP input files ("KPOINTS", "POTCAR", "POSCAR") from the director
 - `ignore::Vector{String}`: An optional list of filenames to ignore during the copy process.
 """
 function copy_vasp_input(path, folder; ignore=String[])
-    for file in ["KPOINTS", "POTCAR", "POSCAR"]
+    for file in ["KPOINTS", "POTCAR", "POSCAR", "INCAR"]
         if !isfile(path*file) && file ∉ ignore
             @info "$file file was not found in current path ($path)."
         end
-        cp(path*file, path*folder*"/$file", force=true)
+        if file ∉ ignore; cp(path*file, path*folder*"/$file", force=true); end
     end
 end
