dff87d385e64adda86e5dcf0c6c9ea3b03ab84bb
martin <ga63loc@mytum.de>
2024-07-30 09:41:21 +0200

    new file for nscf tasks

--- src/tasks/convergence_task.jl
+++ src/tasks/convergence_task.jl
@@ -42,10 +42,3 @@ function run_task_recursive(::Type{Val{:convergence}}, ::Type{Val{:plot}}, args)
     end
     plot_value_convergence(x_par_name, args["par"], x_values, y_values, args["o"])
 end
-
-function run_task(::Type{Val{:nscf}}, ::Type{Val{:make}}, args)
-    nscf_create_subdirectories(args["p"], args["kpoints"])
-    filename = "run_nscf.sh"
-    write_run_script(args["vasp_exe"], args["p"], cb="ln scf/CHGCAR nscf/CHGCAR", out=filename)
-    add_path_to_folders.(filename, ["scf", "nscf"])
-end
