fbb0eb0229325f207f0159d53c037b2ec2e717da
martin <ga63loc@mytum.de>
2024-07-31 12:52:13 +0200

    renamed task

--- src/tasks/run_script.jl
+++ src/tasks/run_script.jl
@@ -2,9 +2,9 @@
 list of available tasks:
 
 run_script
-    creates a bash script that runs vasp in a specific folder
+    make: creates a bash script that runs vasp in a specific folder
 """
 
-function run_task(::Type{Val{:run_script}}, subtask, args)
+function run_task(::Type{Val{:run_script}}, ::Type{Val{:make}}, args)
     write_run_script(args["vasp_exe"], args["p"])
 end
