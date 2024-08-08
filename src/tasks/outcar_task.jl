7fc1a221ce323da1d03bc119049bfa7c99e07b3a
martin <ga63loc@mytum.de>
2024-08-08 14:48:19 +0200

    removed outdated docstring

--- src/tasks/outcar_task.jl
+++ src/tasks/outcar_task.jl
@@ -6,9 +6,7 @@ outcar
 """
 
 
-"""
-reads last value in OUTCAR of parameter for each directory
-"""
+
 function run_task(::Type{Val{:outcar}}, ::Type{Val{:read}}, args)
     values = read_value_from_outcar(args["par"], args["p"]*args["outcar"])
     return values
