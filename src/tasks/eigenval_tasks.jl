0087521045d6ec4f2991dd5981ccdd0b8353d810
martin <ga63loc@mytum.de>
2024-10-17 13:21:19 +0200

    added eigenval read task, works also recursively, to store eigenvalue data in an HDF5 file

--- src/tasks/eigenval_tasks.jl
+++ src/tasks/eigenval_tasks.jl
@@ -12,7 +12,11 @@ function run_task(::Type{Val{:eigenval}}, ::Type{Val{:plot}}, args)
 end
 
 function run_task(::Type{Val{:eigenval}}, ::Type{Val{:read}}, args)
-    output_filename = args["o"]
     input_filename = args["p"] * args["eigenval"]
-    kp, Es, _ = read_eigenval(input_filename)
+    output_filename = args["o"] == "none" ? "eigenval.h5" : args["o"]
+    if occursin("h5", output_filename)
+        write_eigenval_to_hdf5(input_filename, output_filename)
+    else
+        throw("Unknown output file format.")
+    end
 end
