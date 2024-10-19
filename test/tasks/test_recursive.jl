6b7e96fb29c23dcc763c1b7cf93dfceba3a9fce0
martin <ga63loc@mytum.de>
2024-10-19 08:32:41 +0200

    this finally fixes it?

--- test/tasks/test_recursive.jl
+++ test/tasks/test_recursive.jl
@@ -10,7 +10,7 @@ path_recursive = path * "convergence/"
 
 task = Val{Symbol("calculate")}
 subtask = Val{Symbol("bandgap")}
-args = Dict{String, String}("r"=>true, "p" => path_recursive, "eigenval" => "EIGENVAL", "o"=>"none")
+args = Dict{String, Any}("r"=>true, "p" => path_recursive, "eigenval" => "EIGENVAL", "o"=>"none")
 
 output = @capture_out run_task_recursive(task, subtask, args)
 @test output == "ΔE = 0.5946059999999997\nΔE = 0.595008\nΔE = 0.594897\n" 
