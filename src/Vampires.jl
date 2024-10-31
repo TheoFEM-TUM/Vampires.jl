03d1af64e2008522c3ce06dbe9c98629ee55447c
martin <ga63loc@mytum.de>
2024-10-31 11:12:50 +0100

    added ChunkSplitters dep

--- src/Vampires.jl
+++ src/Vampires.jl
@@ -1,6 +1,6 @@
 module Vampires
 
-using OrderedCollections, Plots, LinearAlgebra, StatsBase, HDF5
+using OrderedCollections, Plots, LinearAlgebra, StatsBase, HDF5, ChunkSplitters
 import Documenter: @doc
 
 include("io/read_utils.jl")
