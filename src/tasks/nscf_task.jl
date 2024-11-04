cb3cfe519366246d0136b4d39eebaddbaa45dcd0
martin <ga63loc@mytum.de>
2024-11-04 14:55:09 +0100

    nscf make task now returns nothing

--- src/tasks/nscf_task.jl
+++ src/tasks/nscf_task.jl
@@ -46,4 +46,5 @@ function run_task(::Type{Val{:nscf}}, ::Type{Val{:make}}, args)
     filename = args["p"]*"run_nscf.sh"
     write_run_script(args["vasp_exe"], args["p"], cb="ln scf/CHGCAR nscf/CHGCAR", out=filename)
     add_path_to_folders.(filename, ["scf", "nscf"])
+    return nothing
 end
