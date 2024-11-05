44033aacd9d1e1b70fefbe8b04428d9cf1539309
martin <ga63loc@mytum.de>
2024-11-05 15:21:13 +0100

    extended doscar read task to account for any shape of pdos

--- src/tasks/doscar_task.jl
+++ src/tasks/doscar_task.jl
@@ -17,22 +17,25 @@ Available commands:
 run_task(::Type{Val{:doscar}}, ::Type{Val{:none}}, args) = nothing
 
 """
-    vamp doscar read [--doscar <file>] [--o <output_filename>]
+    vamp [-r] doscar read [--p <path>] [--doscar <doscar_file>] [--par <parameter>] [--o <output_filename>] [--poscar <poscar_file>] [--incar <incar_file>]
 
 Reads the density of states (DOS) data from a DOSCAR file and writes the results to an HDF5 file if specified.
 
 # Arguments
+- `p`: The path to the working directory.
 - `doscar`: Path to the DOSCAR file to read from.
-- `o`: Output filename where the processed data will be saved. If the filename contains 'h5', the data is saved in HDF5 format.
-
-# Behavior
-- Reads the DOS data from the specified DOSCAR file.
-- If the output file is in HDF5 format, it writes the energy, DOS, and integrated DOS (IDOS) data to the specified file in HDF5 format.
+- `par`: An optional parameter that specifies what to read from the doscar (e.g., pdos)
+- `o`: Output filename where the processed data will be saved.
+- `poscar`: Path to the POSCAR file (needed for atom types).
+- `incar`: Path to the INCAR file (needed for parameters that affect the (p)dos).
 
 # Examples
 ```bash
 # Example 1: Read DOSCAR data and save it to an HDF5 file.
-vamp doscar read --doscar DOSCAR --o output.h5
+vamp doscar read --doscar DOSCAR --o dos.h5
+
+# Example 2: Read the pdos from the DOSCAR file and save it to an HDF5 file.
+vamp doscar read --par pdos --o pdos.h5
 """
 function run_task(::Type{Val{:doscar}}, ::Type{Val{:read}}, args)
     doscar = joinpath(args["p"], args["doscar"])
@@ -40,16 +43,24 @@ function run_task(::Type{Val{:doscar}}, ::Type{Val{:read}}, args)
     keys = String["energy", "total_dos", "integrated_dos"]
     values = Vector{Float64}[dos[:, 1], dos[:, 2], dos[:, 3]]
     if args["par"] == "pdos"
+        # Read atom types from POSCAR
         atom_types = read_poscar(joinpath(args["p"], args["poscar"])).atom_types
         atom_types = add_atom_counts(atom_types)
-        orbitals = get_pdos_orbital_list()
+
+        # Read relevant INCAR parameters from INCAR
+        incar = read_incar(joinpath(args["p"], args["incar"]))
+        LORBIT = haskey(incar, "LORBIT") ? parse(Int64, findvalue(incar, "LORBIT")) : 0
+        ISPIN = haskey(incar, "ISPIN") ? parse(Int64, findvalue(incar, "ISPIN")) : 1
+        LSORBIT = haskey(incar, "LSORBIT") ? parse(Bool, findvalue(incar, "LSORBIT")) : false
+        LMAXMIX = haskey(incar, "LMAXMIX") ? parse(Int64, findvalue(incar, "LMAXMIX")) : 2
+        orbitals = get_pdos_orbital_list(LORBIT=LORBIT, ISPIN=ISPIN, LSORBIT=LSORBIT, LMAXMIX=LMAXMIX)
+
         _, pdos, _ = read_doscar_with_pdos(doscar)
         for (i, type) in enumerate(atom_types), (j, orbital) in enumerate(orbitals)
             push!(keys, "$type"*"_"*"$orbital")
             push!(values, pdos[i][1+j, :])
         end
     end
-    @show keys
     return keys, values
 end
 
