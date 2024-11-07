60fe1c33d065aef36ccfae6ac551c10d973721f2
martin <ga63loc@mytum.de>
2024-11-07 11:53:51 +0100

    added new keywords for plotting

--- src/cli_interface.jl
+++ src/cli_interface.jl
@@ -134,6 +134,11 @@ function get_default_args()
         "nsim" => "none",
         "kpar" => "none",
         "super_cell_vector" => "none",
+        "title" => "",
+        "xdata" => "",
+        "ydata" => "",
+        "xlabel" => "",
+        "ylabel" => ""
     )
     return args_dict
 end
@@ -168,7 +173,12 @@ function get_arg_description()
         "ncore" => "Vector of numbers of CPU cores to use for the simulation (scaling tasks only)",
         "nsim" => "Vector of numbers of bands to work on concurrently (scaling tasks only)",
         "kpar" => "Vector of numbers of k-point parallel divisions for the simulation. Determines the parallelization over k-points (scaling tasks only)",
-        "super_cell_vector" => "Vector of the first supercell in weak scaling. Nth supercell is then created according to n*super_cell_vector (weak scaling tasks only)"
+        "super_cell_vector" => "Vector of the first supercell in weak scaling. Nth supercell is then created according to n*super_cell_vector (weak scaling tasks only)",
+        "title" => "specifies the title of a plot",
+        "xdata" => "specifies the xdata for a plot",
+        "ydata" => "specifies the ydata for a plot",
+        "xlabel" => "specifies the label of the x axis",
+        "ylabel" => "specifies the label of the y axis"
     )
     return arg_descriptions
 end
