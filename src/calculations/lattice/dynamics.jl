28fe0a9ed7ac480522177bf1d095d33e8add4b25
Jonas <joldenstaedt@gmail.com>
2024-11-08 17:37:53 +0100

    cosmetics

--- src/calculations/lattice/dynamics.jl
+++ src/calculations/lattice/dynamics.jl
@@ -1,11 +1,11 @@
 """
     get_msd(r⃗₀, r⃗ᵢ, lattice)
 
-Calculates the mean-squared displacement (MSD) and its associated error over time from an initial atomic configuration (`r⃗₀`) and a series of subsequent atomic configurations (`r⃗ᵢ`) in a molecular dynamics simulation, accounting for periodic boundary conditions.
+Calculates the mean-squared displacement (MSD) and its associated error over time from an initial atomic configuration (`r₀`) and a series of subsequent atomic configurations (`rᵢ`) in a molecular dynamics simulation, accounting for periodic boundary conditions.
 
 # Arguments
-- `r⃗₀::Matrix{Float64}`: Initial atomic positions as a 3xN matrix, where `N` is the number of atoms.
-- `r⃗ᵢ::Array{Float64, 3}`: Atomic positions over time as a 3xNxt matrix, where `t` is the number of time steps.
+- `r₀::Matrix{Float64}`: Initial atomic positions as a 3xN matrix, where `N` is the number of atoms.
+- `rᵢ::Array{Float64, 3}`: Atomic positions over time as a 3xNxt matrix, where `t` is the number of time steps.
 - `lattice::Matrix{Float64}`: Lattice vectors defining the periodic boundary conditions, represented as a 3x3 matrix.
 
 # Returns
@@ -13,13 +13,13 @@ Calculates the mean-squared displacement (MSD) and its associated error over tim
 - `err::Vector{Float64}`: The standard deviation of displacements for each time step, representing the error in the MSD calculation.
 
 """
-function get_msd(r⃗₀, r⃗ᵢ, lattice)
+function get_msd(r₀, rᵢ, lattice)
     Rs = hcat([[i, j, k] for i in -1:1, j in -1:1, k in -1:1]...)
     Ts = frac_to_cart(Rs, lattice)
     err = Float64[]
-    @views msd = map(axes(r⃗ᵢ, 3)) do t
-        sum_square_displacements = map(axes(r⃗₀, 2)) do i
-            minimum([sum((r⃗₀[:, i] .- r⃗ᵢ[:, i, t] .+ T⃗).^2) for T⃗ in eachcol(Ts)])
+    @views msd = map(axes(rᵢ, 3)) do t
+        sum_square_displacements = map(axes(r₀, 2)) do i
+            minimum([sum((r₀[:, i] .- rᵢ[:, i, t] .+ T⃗).^2) for T⃗ in eachcol(Ts)])
         end
         push!(err, std(sum_square_displacements))
         mean(sum_square_displacements)
