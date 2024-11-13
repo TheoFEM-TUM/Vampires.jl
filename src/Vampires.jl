87856f2d55512dacbfdf69414e8b709cf001ddbe
martin <ga63loc@mytum.de>
2024-11-13 15:15:58 +0100

    added tests for the effective mass of GaAs

--- src/Vampires.jl
+++ src/Vampires.jl
@@ -42,7 +42,7 @@ export write_to_file, read_from_file, write_kpoints
 export plot_bandstructure, plot_value_convergence
 
 export convergence_create_subdirectories, nscf_create_subdirectories, write_run_script, add_path_to_folders, supercell_create_subdirectories
-export get_bandgap, get_vbm_and_cbm, get_fermi_energy
+export get_bandgap, get_vbm_and_cbm, get_fermi_energy, get_effective_mass
 export frac_to_cart, cart_to_frac, get_volume, get_bs, transform_primitive_cell
 export compute_dos, convert_kspacing_to_kgrid
 
--- test/calculations/test_bandstructure.jl
+++ test/calculations/test_bandstructure.jl
@@ -40,5 +40,29 @@ end
 
     # Test 10: Edge case N = 8 (maximum valid N)
     @test Vampires.get_finite_difference_coef(8) == [469/90, -223/10, 879/20, -949/18, 41, -201/10, 1019/180, -7/10]
+end
 
+@testset "Effective mass" begin
+    kp, Es, occs = read_eigenval(test_file_path*"EIGENVAL_gaas")
+    poscar = read_poscar(test_file_path*"POSCAR_gaas")
+
+    # Literature values from https://arxiv.org/pdf/1601.05300.pdf
+
+    # Test 1: Test heavy hole mass
+    m_hp = get_effective_mass(kp[:, 21:24], Es[4, 21:24], poscar.lattice)
+    m_hf = get_effective_mass(kp[:, 21:24], Es[4, 21:24], poscar.lattice, method="fdm")
+    @test abs(m_hp - m_hf) < 0.1
+    @test abs(m_hp - -0.359) < 0.05
+
+    # Test 2: Test electron mass
+    m_ep = get_effective_mass(kp[:, 21:24], Es[5, 21:24], poscar.lattice)
+    m_ef = get_effective_mass(kp[:, 21:24], Es[5, 21:24], poscar.lattice, method="fdm")
+    @test abs(m_ep - m_ef) < 0.05
+    @test abs(m_ep - 0.077) < 0.05
+
+    # Test 3: Test light hole mass
+    m_hp = get_effective_mass(kp[:, 21:24], Es[2, 21:24], poscar.lattice)
+    m_hf = get_effective_mass(kp[:, 21:24], Es[2, 21:24], poscar.lattice, method="fdm")
+    @test abs(m_hp - m_hf) < 0.05
+    @test abs(m_hp - -0.076) < 0.05
 end
