e78315efe4d1ac1ca87748c36b37e97a0b091699
martin <ga63loc@mytum.de>
2024-11-18 22:50:37 +0100

    reordered colors again because orange default plots dont look so nice

--- src/plotting/vamp_colors.jl
+++ src/plotting/vamp_colors.jl
@@ -21,10 +21,10 @@ mycolor = vcolors[:yellow]
 ```
 """
 const vcolors = (
-    orange = RGB(0.90, 0.60, 0.0),
     blue = RGB(0.0, 0.45, 0.70),
-    yellow = RGB(0.95, 0.90, 0.25),
+    orange = RGB(0.90, 0.60, 0.0),
     green = RGB(0.0, 0.60, 0.50),
+    yellow = RGB(0.95, 0.90, 0.25),
     purple = RGB(0.80, 0.60, 0.70),
     sky_blue = RGB(0.35, 0.70, 0.90),
     vermilion = RGB(0.80, 0.40, 0.0),
