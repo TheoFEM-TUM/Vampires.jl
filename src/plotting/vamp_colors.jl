4de6d1f167c64585832ef1253447e2388005abc1
martin <ga63loc@mytum.de>
2024-11-18 20:30:02 +0100

    Colors can now be set automatically by envoking `autocolor()`. Color counter is reset with `resetcolor()`

--- src/plotting/vamp_colors.jl
+++ src/plotting/vamp_colors.jl
@@ -11,3 +11,33 @@ const vcolors = (
     vermilion = RGB(0.80, 0.40, 0.0),
     black = RGB(0.0, 0.0, 0.0)
 )
+
+"""
+    create_color_getter(vcolors::Vector)
+
+Creates a color-cycling mechanism that iterates through a given list of colors in `vcolors`.
+
+This function returns two closures:
+1. `autocolor`: Retrieves the next color from the list in sequence. It wraps around to the 
+   beginning after reaching the last color, ensuring infinite cycling.
+2. `resetcolor`: Resets the sequence so that the next call to `get_color` starts from the 
+   first color in the list.
+
+# Returns
+- `autocolor::Function`: A function that returns the next color in sequence.
+- `resetcolor::Function`: A function to reset the sequence to start from the first color.
+"""
+function create_color_getter()
+    index = 0
+    function autocolor() 
+        index = mod(index, length(vcolors)) + 1 
+        return vcolors[index]
+    end 
+    function resetcolor() 
+        index = 0 
+    end 
+    return autocolor, resetcolor
+end
+
+# Assign the closure to a global constant
+const autocolor, resetcolor = create_color_getter()
