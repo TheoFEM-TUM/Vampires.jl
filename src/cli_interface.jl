e15a6187c48dd72d0f301d3ddc7cd823c02b52af
martin <ga63loc@mytum.de>
2024-11-19 16:23:16 +0100

    missing comma

--- src/cli_interface.jl
+++ src/cli_interface.jl
@@ -148,7 +148,7 @@ function get_default_args()
         "xdata" => "",
         "ydata" => "",
         "xlabel" => "",
-        "ylabel" => ""
+        "ylabel" => "",
         "partition" => "batch",
         "time" => "1",
         "nodes" => "1",
