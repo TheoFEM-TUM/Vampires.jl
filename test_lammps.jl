459df3eb4e9aca17042b9aa106bf8433ffd9fdf2
frevon <44679888+frevon@users.noreply.github.com>
2024-12-04 16:11:45 +0100

    Delete test_lammps.jl

--- test_lammps.jl
+++
@@ -1,7 +0,0 @@
-using Vampires
-
-
-
-structure = read_lammps("test/test_files/position_type.lammpstrj")
-println(structure.positions)
-write_to_file(structure.positions, "test/test_files/configs_lammps_type")
