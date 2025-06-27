bf40578bffed5bd368eeb5900ab1c115aeb4c826
martin <ga63loc@mytum.de>
2025-06-27 11:20:32 +0200

    fixed function reference

--- test/io/test_command_log.jl
+++ test/io/test_command_log.jl
@@ -1,12 +1,12 @@
 @testset "Command logging" begin
     # Test 1: Test args to command conversion
-    @test get_command_from_cli_args(["--input", "file.txt"]) == "vamp --input file.txt"
+    @test Vampires.get_command_from_cli_args(["--input", "file.txt"]) == "vamp --input file.txt"
 
-    @test get_command_from_cli_args(["run", "--flag", "true"]) == "vamp run --flag true"
+    @test Vampires.get_command_from_cli_args(["run", "--flag", "true"]) == "vamp run --flag true"
 
-    @test get_command_from_cli_args(["--value", "42", "-v"]) == "vamp --value 42 -v"
+    @test Vampires.get_command_from_cli_args(["--value", "42", "-v"]) == "vamp --value 42 -v"
 
-    @test get_command_from_cli_args([]) == "vamp "
+    @test Vampires.get_command_from_cli_args([]) == "vamp "
 
-    @test get_command_from_cli_args(["--path", "/some/dir/with spaces"]) == "vamp --path /some/dir/with spaces"
+    @test Vampires.get_command_from_cli_args(["--path", "/some/dir/with spaces"]) == "vamp --path /some/dir/with spaces"
 end
