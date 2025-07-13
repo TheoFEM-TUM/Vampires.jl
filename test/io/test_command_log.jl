ee2b392ff27a7f9f39c7e224ee85565132c9f481
martin <ga63loc@mytum.de>
2025-07-13 10:10:46 +0200

    added test for command log

--- test/io/test_command_log.jl
+++ test/io/test_command_log.jl
@@ -9,4 +9,24 @@
     @test Vampires.get_command_from_cli_args([]) == "vamp "
 
     @test Vampires.get_command_from_cli_args(["--path", "/some/dir/with spaces"]) == "vamp --path /some/dir/with spaces"
+
+    mktemp() do temp_logfile, io
+        command = "run_simulation"
+        args = Dict("log" => "local")
+
+        # Call the function with a temporary log file
+        Vampires.write_command_to_logfile(command, args; filename=temp_logfile)
+
+        # Read the log file contents
+        log_contents = read(temp_logfile, String)
+
+        # Check that the command is logged
+        @test occursin(command, log_contents)
+
+        # Check that the user name is in the log
+        @test occursin(ENV["USER"], log_contents)
+
+        # Check timestamp format (rough match for yyyy-mm-dd)
+        @test occursin(r"\d{4}-\d{2}-\d{2}", log_contents)
+    end
 end
