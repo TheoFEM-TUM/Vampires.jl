2b270e121a9d7398a42a7774dc8097b7219f319e
Frederik Vonhoff <frederik.vonhoff@tum.de>
2025-09-23 11:20:52 +0200

    correction of documentation for command vamp lammps read

--- src/tasks/lammps_tasks.jl
+++ src/tasks/lammps_tasks.jl
@@ -16,20 +16,21 @@ Available commands:
 run_task(::Type{Val{:lammps}}, ::Type{Val{:none}}, args) = nothing
 
 """
-    vamp [-r] lammps read --lmp_file <file> [--p <path>]
+    vamp [-r] lammps read --lmp_file <file> [--p <path>] [--o <file>]
 
-Reads atomic configurations from an XDATCAR file and calculates specific properties, such as mean squared displacement (MSD), based on the specified parameter.
+Reads atomic configurations from an LAMMPS file and calculates specific properties, such as mean squared displacement (MSD), based on the specified parameter.
 
 # Arguments
 - `lmp_file`: The name of the LAMMPS file containing atomic configurations from a molecular dynamics simulation.
-- `p`: (Optional) The path where the XDATCAR and POSCAR files are located.
+- `p`: (Optional) The path where the LAMMPS files are located.
+- `o`: (Optional) The name of the output file.
 
 # Returns
 - Returns the lattice vectors and configurations from the XDATCAR file.
 
 # Examples
 # Read atomic configurations and output the lattice vectors and the positions
-vamp lammps read --lmp_file position.lammpstrj --p /home/LAMMPS_output/
+vamp lammps read --lmp_file position.lammpstrj --p /home/LAMMPS/ --o structures.h5
 ```
 """
 function run_task(::Type{Val{:lammps}}, ::Type{Val{:read}}, args)
